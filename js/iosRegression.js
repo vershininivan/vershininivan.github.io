854d951faef31abb68a492a543aef6c068ba6431
Ivan Vershinin <vershinin-iv@yandex.ru>
2017-08-26 17:26:31 +0300

    regression#4

--- js/iosRegression.js
+++ js/iosRegression.js
@@ -4,8 +4,10 @@ window.onload = function () {
     $(document).ready(function () {
         $.getJSON("src/regressionTable.json", function (arrayDevice) {
 
-            console.log(arrayDevice.data.structure.fields[0].title);
-
+            //console.log(arrayDevice.data.structure.fields[0].title);
+            for (var i = 0; i < arrayDevice.data.structure.fields.length; i++) {
+                console.log(arrayDevice.data.structure.fields[i].title);
+            }
         });
     });
 }
