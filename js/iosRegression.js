774a030d8766d9309e1f1ba49a574b1d3b3ccb7e
Ivan Vershinin <vershinin-iv@yandex.ru>
2017-08-26 17:38:16 +0300

    regression#5

--- js/iosRegression.js
+++ js/iosRegression.js
@@ -1,13 +1,36 @@
+
+
+
+var table = '<div id="table">';
+var row = '<div class="row">';
+var cellOpen = '<span class="cell">';
+var cellClose = '</span>';
+var divClose = '</div>';
+
+
 var arrayDeviceIsUsed = [];
 
 window.onload = function () {
     $(document).ready(function () {
         $.getJSON("src/regressionTable.json", function (arrayDevice) {
 
+            var text_1 = text_2 = text_3 = "";
+
+            text_1 += table + row;
+
             //console.log(arrayDevice.data.structure.fields[0].title);
+
             for (var i = 0; i < arrayDevice.data.structure.fields.length; i++) {
-                console.log(arrayDevice.data.structure.fields[i].title);
+
+                text_1 += cellOpen + arrayDevice.data.structure.fields[i].title + cellClose;
+
+                //console.log(arrayDevice.data.structure.fields[i].title);
             }
+
+            text_1 += divClose + divClose;
+
+            $(".smallTable").html(text_1);
+
         });
     });
 }
