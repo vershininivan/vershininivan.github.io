c27858a808ca05cc4c5d538c36b0b98acf3b55e5
Ivan Vershinin <vershinin-iv@yandex.ru>
2017-08-26 18:09:26 +0300

    regression#11

--- js/iosRegression.js
+++ js/iosRegression.js
@@ -1,14 +1,10 @@
 
-
-
 var tableOpen = '<div id="table">';
 var tableClose = '</div>';
 var rowOpen = '<div class="row">';
 var rowClose = '</div>';
 var cellOpen = '<span class="cell">';
 var cellClose = '</span>';
-
-
 
 var arrayDeviceIsUsed = [];
 
@@ -26,18 +22,28 @@ window.onload = function () {
 
                 text_1 += cellOpen + arrayDevice.data.structure.fields[i].title + cellClose;
 
-                //console.log(arrayDevice.data.structure.fields[i].title);
+            }
+
+            for (var i = 0; i < arrayDevice.data.rows.length; i++) {
+
+                var rowArray = arrayDevice.data.rows[i];
+
+                for (var j = 0; j < rowArray.length; j++) {
+                    text_1 += cellOpen + rowArray[0].sort + cellClose;
+                }
+
             }
 
             text_1 += tableClose + rowClose;
 
             $(".smallTable").html(text_1);
 
-            console.log(arrayDevice.data.rows.length);
-            console.log(arrayDevice.data.rows[0].length);
-            var rowArray = arrayDevice.data.rows[0];
-            console.log(rowArray[0].sort);
-            
+
+            //console.log(arrayDevice.data.rows.length);
+            //console.log(arrayDevice.data.rows[0].length);
+            //var rowArray = arrayDevice.data.rows[0];
+            //console.log(rowArray[0].sort);
+
 
         });
     });
