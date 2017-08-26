d6c9dbd8b90e6933064d9833758806da84935695
Ivan Vershinin <vershinin-iv@yandex.ru>
2017-08-26 23:50:57 +0300

    regression#16

--- js/iosRegression.js
+++ js/iosRegression.js
@@ -8,65 +8,51 @@ var rowClose = '</div>';
 var cellOpen = '<div class="divTableCell">';
 var cellClose = '</div>';
 
-/*var tableOpen = '<table border="1">';
-var tableClose = '</table>';
-var rowOpen = '<tr>';
-var rowClose = ' </tr>';
-var cellOpen = '<td>';
-var cellClose = '</td>';*/
-
-
-var arrayDeviceIsUsed = [];
+var tableArray = [];
+var text_1 = "";
 
 window.onload = function () {
     $(document).ready(function () {
-        $.getJSON("src/regressionTable.json", function (arrayDevice) {
+        $.getJSON("src/regressionTable.json", function (jsonToArray) {
 
-            var text_1 = text_2 = text_3 = "";
+            tableArray = jsonToArray;
 
-            text_1 += tableOpen;
-
-            //console.log(arrayDevice.data.structure.fields[0].title);
-
-            text_1 += rowOpen;
-
-            for (var i = 0; i < arrayDevice.data.structure.fields.length; i++) {
-
-                text_1 += cellOpen + arrayDevice.data.structure.fields[i].title + cellClose;
-
-            }
-
-            text_1 += rowClose;
-
-            for (var i = 0; i < arrayDevice.data.rows.length; i++) {
-
-                var rowArray = arrayDevice.data.rows[i];
-
-                text_1 += rowOpen;
-
-                for (var j = 0; j < rowArray.length; j++) {
-                    text_1 += cellOpen + rowArray[j].sort + cellClose;
-                }
-
-                text_1 += rowClose;
-
-            }
-
-            text_1 += tableClose;
-
-            $(".smallTable").html(text_1);
-
-
-            console.log(arrayDevice.data.rows.length);
-            console.log(arrayDevice.data.rows[0].length);
-            var rowArray = arrayDevice.data.rows[0];
-            console.log(rowArray[0].sort);
-            console.log(rowArray[1].sort);
-            console.log(rowArray[2].sort);
-            console.log(rowArray[3].sort);
-
+            generateTable();
 
         });
     });
 }
 
+function generateTable() {
+
+    text_1 += tableOpen;
+
+    text_1 += rowOpen;
+
+    for (var i = 0; i < tableArray.data.structure.fields.length; i++) {
+
+        text_1 += cellOpen + tableArray.data.structure.fields[i].title + cellClose;
+
+    }
+
+    text_1 += rowClose;
+
+    for (var i = 0; i < tableArray.data.rows.length; i++) {
+
+        var rowArray = tableArray.data.rows[i];
+
+        text_1 += rowOpen;
+
+        for (var j = 0; j < rowArray.length; j++) {
+            text_1 += cellOpen + rowArray[j].sort + cellClose;
+        }
+
+        text_1 += rowClose;
+
+    }
+
+    text_1 += tableClose;
+
+    $(".smallTable").html(text_1);
+
+}
