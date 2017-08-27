4c048a8a428111e13b1fb5da963916d25060c625
Ivan Vershinin <vershinin-iv@yandex.ru>
2017-08-27 14:59:41 +0300

    fdgsdfgs

--- js/iosRegression.js
+++ js/iosRegression.js
@@ -127,7 +127,7 @@ function testersInRegression() {
 
     for (var i = 0; i < tableRawArray.length; i++) {
 
-        console.log(tableRawArray.length);
+        console.log(tableRawArray[i][0].length);
 
         RegexVar = tableRawArray[i][6].match(/[А-Яа-я]*/ig);
         testArrayName.push(RegexVar);
