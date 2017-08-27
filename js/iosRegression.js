77e3d0dfcc4fdb56e2b70d056c2a4f83eee82915
Ivan Vershinin <vershinin-iv@yandex.ru>
2017-08-27 15:58:31 +0300

    sdhdgh

--- js/iosRegression.js
+++ js/iosRegression.js
@@ -148,17 +148,18 @@ function testersInRegression() {
 
     }
 
+    var m = 0;
+
     for (var i = 0; i < testArrayTime.length; i++) {
         var k = 0;
-        var m = 0;
         testArraySortTestersTime[i] = [];
         for (var j = 0; j < testArrayTime[i].length; j++) {    
             if (typeof testArrayTime[i][j] !== undefined && testArrayTime[i][j] !== null && testArrayTime[i][j] !== "") {
-                testArraySortTestersTime[i][k] =testArrayTime[i][j];
+                testArraySortTestersTime[m][k] =testArrayTime[i][j];
                 k++;
             }
         }
-        //m++;
+        m++;
     }
 
     console.log(testArrayName);
