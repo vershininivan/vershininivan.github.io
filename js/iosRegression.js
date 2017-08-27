36927a078f941d9a2e49f28d4c197da2f6039d22
Ivan Vershinin <vershinin-iv@yandex.ru>
2017-08-27 15:33:06 +0300

    fghdfgjh

--- js/iosRegression.js
+++ js/iosRegression.js
@@ -139,7 +139,7 @@ function testersInRegression() {
         var k = 0;
         for (var j = 0; j < testArrayName[i].length; j++) {    
             if (typeof testArrayName[i][j] !== undefined && testArrayName[i][j] !== null && testArrayName[i][j] !== "") {
-                testArraySortTestersName[i][k].push(testArrayName[i][j]);
+                testArraySortTestersName[i].push(testArrayName[i][j]);
                 k++;
             }
         }
