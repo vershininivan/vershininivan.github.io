7e91e6695f234796d62a92869e81ba9225c93b41
Ivan Vershinin <vershinin-iv@yandex.ru>
2017-08-28 09:38:51 +0300

    reg$70

--- js/iosRegression.js
+++ js/iosRegression.js
@@ -176,7 +176,7 @@ function calculateEstimate() {
     for (var i = 0; i < testersInRegressionSortArray.length; i++) {
 
         for (var j = 0; j < testArraySortTestersName.length; j++) {
-            estimateTestersInRegression[i] = [];
+            estimateTestersInRegression[i][i] = [];
             for (var k = 0; k < testArraySortTestersName[j].length; k++) {
                 console.log(testArraySortTestersName[j][k]);
                 console.log(testersInRegressionSortArray[i]);
